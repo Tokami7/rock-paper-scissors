e5fe9ae7bc45d4e0b42d71c9bca8855945b02bbb
Tom <thomas.mant@hotmail.com>
2026-04-26 11:59:49 +0100

    Create function to play one round of the game

--- javascript.js
+++ javascript.js
@@ -23,12 +23,35 @@ function getHumanChoice() {
     }
     return humanOutput;
 }
-console.log(getComputerChoice());
-console.log(getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-
+    if (humanChoice === computerChoice) {
+        console.log("You picked the same. It's a Draw!");
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        console.log("You Win! Rock beats Scissors");
+        humanScore =+ 1;
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        console.log("You Win! Paper beats Rock");
+        humanScore =+ 1;
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        console.log("You Win! Scissors beats Paper");
+        humanScore =+ 1;
+    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        console.log("You Lose! Paper beats Rock");
+        computerScore =+ 1;
+    } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+        console.log("You Lose! Scissors beats Paper");
+        computerScore =+ 1;
+    } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+        console.log("You Lose! Rock beats Scissors");
+        computerScore =+ 1;
+    } 
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection)
